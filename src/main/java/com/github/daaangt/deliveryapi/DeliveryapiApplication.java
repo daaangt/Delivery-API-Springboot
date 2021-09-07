a2a297044cdce8305dabac922d562a587d3df959
daaangt <danlilogalvao@hotmail.com>
2021-09-06 23:50:02 -0300

    Initial Commit

---
+++ src/main/java/com/github/daaangt/deliveryapi/DeliveryapiApplication.java
@@ -0,0 +1,12 @@
+package com.github.daaangt.deliveryapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeliveryapiApplication.class, args);
+    }
+}
