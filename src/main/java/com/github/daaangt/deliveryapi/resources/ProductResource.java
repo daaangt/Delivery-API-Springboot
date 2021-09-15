0ba9f2d1a372291d2ec082f95d893ecb564091f1
Danilo Galvão <59860240+daaangt@users.noreply.github.com>
2021-09-14 21:38:38 -0300

    Product paths

--- src/main/java/com/github/daaangt/deliveryapi/resources/ProductResource.java
+++ src/main/java/com/github/daaangt/deliveryapi/resources/ProductResource.java
@@ -1,23 +1,24 @@
 package com.github.daaangt.deliveryapi.resources;
 
+import com.github.daaangt.deliveryapi.entities.Order;
 import com.github.daaangt.deliveryapi.entities.Product;
 
+import java.time.Instant;
 import java.util.List;
 
 import com.github.daaangt.deliveryapi.repositories.ProductRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductResource {
-
     @Autowired
     private ProductRepository repository;
 
@@ -26,10 +27,33 @@ public class ProductResource {
         return repository.findAll();
     }
 
+    @PostMapping
+    public ResponseEntity<Product> store(@Valid @RequestBody Product obj) {
+        repository.save(obj);
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
+
+    @GetMapping(value = "/name/{name}")
+    public Product findByName(@PathVariable String name) {
+        return repository.findByName(name);
+    }
+
     @GetMapping(value = "/{id}")
     public ResponseEntity findById(@PathVariable Long id) {
         return repository.findById(id)
                 .map(record -> ResponseEntity.ok().body(record))
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Product> update(@PathVariable Long id, @Valid @RequestBody Product productDetails) {
+        return repository.findById(id)
+                .map(record -> {
+                    record.setName(productDetails.getName());
+                    record.setPrice(productDetails.getPrice());
+                    record.setStock(productDetails.getStock());
+
+                    return ResponseEntity.ok().body(repository.save(record));
+                }).orElse(ResponseEntity.notFound().build());
     }
 }
